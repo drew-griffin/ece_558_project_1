6735f2ae795f8cae0a8850abc04a44d31aab06bf
drew-griffin <dgriffinseidel@gmail.com>
2023-02-10 18:20:13 -0800

    foundation added

--- app/src/main/java/edu/pdx/dseidel/ece_558_project_1/MainActivity.kt
+++ app/src/main/java/edu/pdx/dseidel/ece_558_project_1/MainActivity.kt
@@ -2,10 +2,69 @@ package edu.pdx.dseidel.ece_558_project_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
+import edu.pdx.dseidel.ece_558_project_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val operand1String = binding.operand1.text.toString().trim()
+        val operand2String = binding.operand2.text.toString().trim()
+
+        val valid : Boolean = (operand1String.isNotBlank() && operand1String.isNotEmpty() && operand2String.isNotBlank() && operand2String.isNotEmpty())
+
+
+        var result : Double
+
+
+        /* Button on click listener functions */
+        binding.clearButton.setOnClickListener {
+
+        }
+        binding.plusButton.setOnClickListener {
+            if (valid){
+                result = operand1String.toDouble() + operand2String.toDouble()
+                binding.finalResult.text = result.toString()
+            }
+
+        }
+        binding.minusButton.setOnClickListener {
+            if (valid){
+                result = operand1String.toDouble() - operand2String.toDouble()
+                binding.finalResult.text = result.toString()
+            }
+        }
+        binding.multiplyButton.setOnClickListener {
+            if (valid){
+                result = operand1String.toDouble() * operand2String.toDouble()
+                binding.finalResult.text = result.toString()
+            }
+        }
+        binding.divideButton.setOnClickListener{
+            if (valid && operand2String.toDouble() != 0.0){
+                result = operand1String.toDouble() * operand2String.toDouble()
+                binding.finalResult.text = result.toString()
+            }
+        }
+        binding.percentButton.setOnClickListener {
+
+        }
+        binding.squareRootButton.setOnClickListener {
+
+        }
+
     }
+
+
+
+
+
 }
